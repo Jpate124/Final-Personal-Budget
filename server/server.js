8f84b07b9cfb2d251313df71f20a576d05efa249
Patel Jeet <you@example.com>
2020-12-17 20:33:35 -0500

    Added JWT Authentication

--- server/server.js
+++ server/server.js
@@ -7,7 +7,8 @@ const app = express();
 const port = process.env.port || 3000;
 
 const bodyParser = require('body-parser');
-const path = require('path');
+const jwt = require('jsonwebtoken');
+const exjwt = require('express-jwt');
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -22,12 +23,18 @@ var connection = mysql.createConnection({
 const budget = require ("./budget");
 const users = require ("./signup");
 
+const secretKey = 'My super secret key';
+const jwtMW = exjwt({
+    secret: secretKey,
+    algorithms: ['HS256']
+});
+
+
 app.post('/budget', (req, res) => {
     const userId = req.body.userId;
     try {
         connection.query('SELECT * FROM Budget Where userId = ?', userId, function (error, results, fields) {
         if (error) throw error;
-            console.log(results);
             res.json(results);
         });
      } catch (e) {
@@ -64,7 +71,14 @@ app.post('/api/login', (req, res) => {
     const {username, password} = req.body;
     connection.query('SELECT * FROM `user` Where username = ? and password = ?', [username, password], function (error, results, fields) {
         if (error) throw error;
-        res.json(results);
+        if(!(results.length  === 0 || results === undefined)) {
+            let token = jwt.sign({ id: results[0].id, username: results[0].username, name: results[0].name }, secretKey, { expiresIn: '7d'});
+            res.json({
+                success: true, 
+                err: null,
+                token
+            });
+        }
     });
 });
 
